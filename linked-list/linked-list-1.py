d704cc51284a6e4829faf4d61898818874f2cc34
Brahati <90918387+Brahati-Adiga@users.noreply.github.com>
2023-01-28 10:18:21 +0530

    done

--- linked-list/linked-list-1.py
+++ linked-list/linked-list-1.py
@@ -1,1 +1,18 @@
+from collections import defaultdict
+Sample_dict = defaultdict(list)
+
+def hash_string(string, size):
+    return sum([ord(literal) for literal in string])%size
+
+
+with open("C:\\Users\\BRAHATI ADIGA\\Downloads\\tt.txt", 'r+') as f:
+    if f.read(1) != '\n':
+        f.flush()
+        f.seek(0)
+    lines = [line[:-1] for line in f]
+for line in lines:
+    print(line)
+    result = hash_string(line, 16)
+    Sample_dict[result].append(line)
+print(Sample_dict)
 
